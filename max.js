128bf840f1c997a9d5e8e572aa0795399c0dc1f1
leslieyi <leslieyi411@gmail.com>
2021-11-30 10:08:58 -0500

    sink down

--- max.js
+++ max.js
@@ -24,6 +24,55 @@ class MaxBinaryHeap {
       //   }
     }
   }
+
+  extractMax() {
+    //no arguments
+    const max = this.values[0];
+    const end = this.values.pop();
+    //take the end and put it in the [0]
+    if (this.values.length > 0) {
+      //edge case
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null; //tracking index with swap and setting it to null when we find a larger number
+
+      //start with the leftside
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break; //break out of the loop
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
@@ -34,6 +83,9 @@ heap.insert(18);
 heap.insert(27);
 heap.insert(12);
 heap.insert(55);
-console.log(heap)
-heap.insert(199)
-console.log(heap)
+console.log(heap);
+heap.insert(199);
+console.log(heap);
+
+console.log("hi", heap.extractMax());
+console.log(heap);
